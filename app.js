44a8692c5a01a490b51bc7c7a08a734788c86638
Tomasz Lacki <tlacki@bizcharger.com>
2020-02-10 15:59:44 +0100

    Added files and changed required to deploy to heroku

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://dbUser:1234@tomekmongodbcluster-0-wg5rr.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://dbUser:1234@tomekmongodbcluster-0-wg5rr.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
